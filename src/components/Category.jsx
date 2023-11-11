b428dc0bb861168d793b8c9485411ae14e0b7afb
Rana <rannaashraf33@gmail.com>
2023-11-11 12:19:04 +0200

    style

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -18,8 +18,18 @@ export default function Category({ header, price, img }) {
       <img src={img} alt="" />
       <h1>{header}</h1>
       <p>{price}</p>
-      <span onClick={handleClickMinus}> -</span>
-      <p>{count}</p> <span onClick={handleClickPlus}> +</span>{" "}
+      <div
+        style={{
+          display: "flex",
+          gap: 10,
+          alignItems: "center",
+          fontSize: 20,
+          fontWeight: "bold",
+        }}
+      >
+        <span onClick={handleClickMinus}> -</span>
+        <p>{count}</p> <span onClick={handleClickPlus}> +</span>
+      </div>
     </div>
   );
 }
