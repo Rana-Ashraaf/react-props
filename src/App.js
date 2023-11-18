0c9f869838415f4a6158e0428eef927396843bb0
Rana <rannaashraf33@gmail.com>
2023-11-18 12:23:51 +0200

    useeffect

--- src/App.js
+++ src/App.js
@@ -5,17 +5,17 @@ function App() {
   const myPizzaArray = [
     {
       name: "Chicken Ranch",
-      price: "5$",
+      price: 5,
       img: "https://images.unsplash.com/photo-1513104890138-7c749659a591?auto=format&fit=crop&q=80&w=1770&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
       name: "Pepperoni",
-      price: "3$",
+      price: 3,
       img: "https://images.unsplash.com/photo-1534308983496-4fabb1a015ee?auto=format&fit=crop&q=80&w=1776&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
       name: "Margherita",
-      price: "2$",
+      price: 2,
       img: "https://images.unsplash.com/photo-1600028068383-ea11a7a101f3?auto=format&fit=crop&q=80&w=1887&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
   ];
--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,7 +1,11 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Category({ header, price, img }) {
   let [count, setMycount] = useState(1);
+  let [addToCart, setAddtoCart] = useState("Add to cart");
+  useEffect(() => {
+    alert(`your total is ${price * count} $`);
+  }, [addToCart]);
 
   let handleClickPlus = () => {
     setMycount((prevState) => {
@@ -13,6 +17,11 @@ export default function Category({ header, price, img }) {
       if (prevState !== 0) return prevState - 1;
     });
   };
+
+  let handleAddToCart = () => {
+    setAddtoCart("Added!");
+  };
+
   return (
     <div className="card">
       <img src={img} alt="" />
@@ -30,6 +39,7 @@ export default function Category({ header, price, img }) {
         <span onClick={handleClickMinus}> -</span>
         <p>{count}</p> <span onClick={handleClickPlus}> +</span>
       </div>
+      <button onClick={handleAddToCart}>{addToCart}</button>
     </div>
   );
 }
